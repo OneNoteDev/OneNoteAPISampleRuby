69e583af6c2d2d253488e526574e00577eb7ec6d
Juan Corona <juanc@evidentpoint.com>
2014-05-07 10:39:16 -0700

    Change camelCase in code to snake_case. Use rails' `present?` method instead of ruby's `nil?` method.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,49 +2,47 @@ class WelcomeController < ApplicationController
   @resourse_url
 
   def index
-    oneNoteClient = OneNoteSharer.new
-    @auth_url = oneNoteClient.authUrl
+    onenote_client = OneNoteSharer.new
+    @auth_url = onenote_client.auth_url
   end
 
   def submit
     @resourse_url = nil
     @response_code = nil
-    oneNoteClient = OneNoteSharer.new
+    onenote_client = OneNoteSharer.new
     access_token = cookies['access_token']
     result = nil
+
     case params[:submit]
       when 'text'
-        result = oneNoteClient.createPageWithSimpleText access_token
+        result = onenote_client.create_page_with_simple_text access_token
       when 'textimage'
-        result = oneNoteClient.createPageWithTextAndImage access_token
+        result = onenote_client.create_page_with_text_and_image access_token
       when 'url'
-        result = oneNoteClient.createPageWithScreenshotFromUrl access_token
+        result = onenote_client.create_page_with_screenshot_from_url access_token
       when 'html'
-        result = oneNoteClient.createPageWithScreenshotFromHtml access_token
+        result = onenote_client.create_page_with_screenshot_from_html access_token
       when 'file'
-        result = oneNoteClient.createPageWithFile access_token
+        result = onenote_client.create_page_with_file access_token
+    end
 
-    end
-    if !result.nil?
-      @resourse_url = result['links']['oneNoteWebUrl']['href']
-    end
+    @resourse_url = result['links']['oneNoteWebUrl']['href'] if result.present?
   rescue Exception => e
     @response = e.response
     @response_code = e.http_code
   end
 
   def callback
-    oneNoteClient = OneNoteSharer.new
-    token_set = oneNoteClient.handleCallbackRequest (params)
-    if !token_set.nil?
+    onenote_client = OneNoteSharer.new
+    token_set = onenote_client.handle_callback_request (params)
+    if token_set.present?
       expire_in = token_set['expires_in'].to_i
       cookies['access_token'] = { :value => token_set['access_token'], :expires => expire_in.seconds.from_now }
       cookies['authentication_token'] = { :value => token_set['authentication_token'], :expires => expire_in.seconds.from_now }
       cookies['scope'] = { :value => token_set['scope'], :expires => expire_in.seconds.from_now }
       refresh_token = token_set['refresh_token']
-      if !refresh_token.nil?
-        oneNoteClient.saveRefreshToken refresh_token
-      end
+
+      onenote_client.save_refresh_token(refresh_token) if refresh_token.present?
     end
   end
 
--- lib/one_note_sharer.rb
+++ lib/one_note_sharer.rb
@@ -9,14 +9,14 @@ class OneNoteSharer
   OAUTH_TOKEN_URL = "https://login.live.com/oauth20_token.srf"
   SHARE_URL = 'https://www.onenote.com/api/v1.0/pages'
 
-  def authUrl
+  def auth_url
     scopes = ["wl.signin", "wl.basic", "wl.offline_access", "office.onenote_create"]
     redirect_url = "#{ONENOTE_CONFIG['callback']}&response_type=code&scope=#{scopes.join(' ')}"
 
-    return "#{OAUTH_AUTHORIZE_URL}?client_id=#{CLIENTID}&display=page&locale=en&redirect_uri=#{URI.escape(redirect_url)}"
+    "#{OAUTH_AUTHORIZE_URL}?client_id=#{CLIENTID}&display=page&locale=en&redirect_uri=#{URI.escape(redirect_url)}"
   end
 
-  def requestAccessToken content
+  def request_access_token content
     response = RestClient.post(OAUTH_TOKEN_URL, content)
     JSON.parse(response)
   rescue Exception => exception
@@ -24,8 +24,8 @@ class OneNoteSharer
   end
 
 
-  def requestAccessTokenByVerifier verifier
-    requestAccessToken (Array(
+  def request_access_token_by_verifier verifier
+    request_access_token (Array(
         'client_id' => CLIENTID,
         'redirect_uri' => CALLBACK,
         'client_secret' => CLIENTSECRET,
@@ -34,135 +34,142 @@ class OneNoteSharer
     ))
   end
 
-  def requestAccessTokenByRefreshToken refreshToken
-    requestAccessToken (Array(
+  def request_access_token_by_refresh refresh_token
+    request_access_token (Array(
         'client_id' => CLIENTID,
         'redirect_uri' => CALLBACK,
         'client_secret' => CLIENTSECRET,
-        'refresh_token' => refreshToken,
+        'refresh_token' => refresh_token,
         'grant_type' => 'refresh_token'
     ))
   end
   
-  
-  def readRefreshToken
+  def read_refresh_token
     # read refresh token of the user identified by the site.
-    return nil
+    nil
   end
 
-  def saveRefreshToken (refreshToken)
+  def save_refresh_token (refreshToken)
     # save the refresh token and associate it with the user identified by your site credential system.
   end
 
-  def handleCallbackRequest (params)
+  def handle_callback_request (params)
     if !params['access_token'].nil?
       return nil
     end
 
     verifier = params['code']
     if !verifier.nil?
-      token_set = requestAccessTokenByVerifier verifier
-      saveRefreshToken token_set['refresh_token']
+      token_set = request_access_token_by_verifier verifier
+      save_refresh_token token_set['refresh_token']
       return token_set
     end
 
-    refreshToken = readRefreshToken
-    if !refreshToken.nil?
-      token_set = requestAccessTokenByRefreshToken refreshToken
-      saveRefreshToken token_set['refresh_token']
+    refresh_token = read_refresh_token
+    if !refresh_token.nil?
+      token_set = request_access_token_by_refresh refresh_token
+      save_refresh_token token_set['refresh_token']
       return token_set
     end
 
-    return nil
-
+    nil
   end
 
-  def getPostHeaders access_token, type=nil
+  def get_post_headers (access_token, type=nil)
     if access_token.nil?
       return false
     end
     #Since cookies are user-supplied content, it must be encoded to avoid header injection
-    encodedAccessToken = URI::escape(access_token)
+    encoded_access_token = URI::escape(access_token)
     if type=='multipart'
-      headers = {:content_type => "multipart/form-data; boundary=#{@boundary}", :'Authorization' => "Bearer #{encodedAccessToken}"}
+      headers = {:content_type => "multipart/form-data; boundary=#{@boundary}", :'Authorization' => "Bearer #{encoded_access_token}"}
     else
-      headers = {:content_type => "text/html", :'Authorization' => "Bearer #{encodedAccessToken}"}
+      headers = {:content_type => "text/html", :'Authorization' => "Bearer #{encoded_access_token}"}
     end
-    return headers
+
+    headers
   end
 
-  def createPageWithSimpleText access_token
-    headers = getPostHeaders access_token
+  def create_page_with_simple_text (access_token)
+    headers = get_post_headers(access_token)
     date = Time.now.to_time.iso8601
 
     html_part = "
       <!DOCTYPE html>
-        <html>
-          <head>
-            <title>A page created from basic HTML-formatted text (PHP Sample)</title>
-            <meta name=\"created\" value=\"#{date}\"/>
-          </head>
-          <body>
-            <p>This is a page that just contains some simple <i>formatted</i> <b>text</b></p>
-          </body>
-        </html>"
+      <html>
+        <head>
+          <title>A page created from basic HTML-formatted text (PHP Sample)</title>
+          <meta name=\"created\" value=\"#{date}\"/>
+        </head>
+        <body>
+          <p>This is a page that just contains some simple <i>formatted</i> <b>text</b></p>
+        </body>
+      </html>"
 
     response = RestClient.post(SHARE_URL, html_part, headers)
-    return JSON.parse (response)
+
+    JSON.parse(response)
   end
 
-  def createPageWithFile access_token
-    headers = getPostHeaders access_token, 'multipart'
+  def create_page_with_file (access_token)
+    headers = get_post_headers(access_token, 'multipart')
     date = Time.now.to_time.iso8601
 
-    html_part = "<!DOCTYPE html>
-    <html>
-      <head>
-        <title>A page with a file on it (Ruby on Rails Sample)</title>
-        <meta name=\"created\" value=\"#{date}\"/>
-      </head>
-      <body>
-        <object data-attachment=\"OneNote Logo.jpg\"
-          data=\"name:embeddedFile\"
-          type=\"image/jpeg\"
-      </body>
+    html_part = "
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>A page with a file on it (Ruby on Rails Sample)</title>
+          <meta name=\"created\" value=\"#{date}\"/>
+        </head>
+        <body>
+          <object data-attachment=\"OneNote Logo.jpg\"
+            data=\"name:embeddedFile\"
+            type=\"image/jpeg\"
+        </body>
       </html>"
 
-    response = RestClient.post SHARE_URL, {:multipart => true,
-                                       :Presentation => html_part,
-                                       :embeddedFile => File.open("Logo.jpg", "rb")
-    } , headers
-    return JSON.parse (response)
+    response = RestClient.post(SHARE_URL, {
+        :multipart => true,
+        :Presentation => html_part,
+        :embeddedFile => File.open('Logo.jpg', 'rb')
+    }, headers)
+
+    JSON.parse(response)
   end
 
-  def createPageWithTextAndImage access_token
-    headers = getPostHeaders access_token, 'multipart'
+  def create_page_with_text_and_image (access_token)
+    headers = get_post_headers(access_token, 'multipart')
     date = Time.now.to_time.iso8601
 
-    html_part = "<!DOCTYPE html>
-    <html>
-      <head>
-        <title>A page created containing an image (Ruby on Rails Sample)</title>
-        <meta name=\"created\" value=\"#{date}\"/>
-      </head>
-      <body>
-        <p>This is a page that just contains some simple <i>formatted</i> <b>text</b> and an image</p>
-        <img src=\"name:imageData\" alt=\"A beautiful logo\" width=\"426\" height=\"68\" />
-      </body>
+    html_part = "
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>A page created containing an image (Ruby on Rails Sample)</title>
+          <meta name=\"created\" value=\"#{date}\"/>
+        </head>
+        <body>
+          <p>This is a page that just contains some simple <i>formatted</i> <b>text</b> and an image</p>
+          <img src=\"name:imageData\" alt=\"A beautiful logo\" width=\"426\" height=\"68\" />
+        </body>
       </html>"
 
-    response = RestClient.post SHARE_URL, {:multipart => true,
-                                       :Presentation => html_part,
-                                       :imageData => File.open("Logo.jpg", "rb")
-    } , headers
-    return JSON.parse (response)
+    response = RestClient.post(SHARE_URL, {
+        :multipart => true,
+        :Presentation => html_part,
+        :embeddedFile => File.open('Logo.jpg', 'rb')
+    }, headers)
+
+    JSON.parse(response)
   end
 
-  def createPageWithScreenshotFromUrl access_token
-    headers = getPostHeaders access_token
+  def create_page_with_screenshot_from_url access_token
+    headers = get_post_headers(access_token)
     date = Time.now.to_time.iso8601
 
-    html_part = "<!DOCTYPE html>
+    html_part = "
+    <!DOCTYPE html>
     <html>
       <head>
         <title>A page created with a URL snapshot on it (Ruby on Rails Sample)</title>
@@ -172,43 +179,48 @@ class OneNoteSharer
         <img data-render-src=\"http://www.onenote.com\" alt=\"An important web page\" />
         Source URL: <a href=\"http://www.onenote.com\">http://www.onenote.com</a>
       </body>
-      </html>"
+    </html>"
 
     response = RestClient.post(SHARE_URL, html_part, headers)
-    return JSON.parse (response)
+
+    JSON.parse(response)
   end
 
-  def createPageWithScreenshotFromHtml access_token
-    headers = getPostHeaders access_token, 'multipart'
+  def create_page_with_screenshot_from_html access_token
+    headers = get_post_headers(access_token, 'multipart')
     date = Time.now.to_time.iso8601
 
-    screenshot = "<html>
-      <head><title>Embedded HTML</title></head>
-      <body>
-        <h1>This is a screen grab of a web page</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula magna quis mauris accumsan, nec imperdiet nisi tempus. Suspendisse potenti.
-        Duis vel nulla sit amet turpis venenatis elementum. Cras laoreet quis nisi et sagittis. Donec euismod at tortor ut porta. Duis libero urna, viverra id
-        aliquam in, ornare sed orci. Pellentesque condimentum gravida felis, sed pulvinar erat suscipit sit amet. Nulla id felis quis sem blandit dapibus. Ut
-        viverra auctor nisi ac egestas. Quisque ac neque nec velit fringilla sagittis porttitor sit amet quam.</p>
-      </body>
+    screenshot = "
+      <html>
+        <head><title>Embedded HTML</title></head>
+        <body>
+          <h1>This is a screen grab of a web page</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula magna quis mauris accumsan, nec imperdiet nisi tempus. Suspendisse potenti.
+          Duis vel nulla sit amet turpis venenatis elementum. Cras laoreet quis nisi et sagittis. Donec euismod at tortor ut porta. Duis libero urna, viverra id
+          aliquam in, ornare sed orci. Pellentesque condimentum gravida felis, sed pulvinar erat suscipit sit amet. Nulla id felis quis sem blandit dapibus. Ut
+          viverra auctor nisi ac egestas. Quisque ac neque nec velit fringilla sagittis porttitor sit amet quam.</p>
+        </body>
       </html>"
 
-    html_part = "<!DOCTYPE html>
-    <html>
-      <head>
-        <title>A page created with a screenshot of HTML on it (Ruby on Rails Sample)</title>
-        <meta name=\"created\" value=\"#{date}\"/>
-      </head>
-      <body>
-        <img data-render-src=\"name:HtmlForScreenshot\" />
-      </body>
+    html_part = "
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>A page created with a screenshot of HTML on it (Ruby on Rails Sample)</title>
+          <meta name=\"created\" value=\"#{date}\"/>
+        </head>
+        <body>
+          <img data-render-src=\"name:HtmlForScreenshot\" />
+        </body>
       </html>"
 
-    response = RestClient.post SHARE_URL, {:multipart => true,
-                                       :Presentation => html_part,
-                                       :HtmlForScreenshot => screenshot
-    } , headers
-    return JSON.parse (response)
+    response = RestClient.post(SHARE_URL, {
+        :multipart => true,
+        :Presentation => html_part,
+        :HtmlForScreenshot => screenshot
+    }, headers)
+
+    JSON.parse(response)
   end
 end
 
